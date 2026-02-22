1911d1c45b8baf708c1e25fedf832edb6ae06d1c
officialnewbharat-art <official.newbharat@gmail.com>
2026-02-23 03:24:01 +0530

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,9 @@
+// proxy.ts
 import { updateSession } from '@/lib/supabase/middleware'
 import { type NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+// Rename this function from 'middleware' to 'proxy'
+export async function proxy(request: NextRequest) {
   return await updateSession(request)
 }
 
